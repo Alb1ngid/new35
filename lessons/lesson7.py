b5750c40d255aa10f1bdfe7287e824461184349a
Geek_Tech <geecktech.och2@gmail.com>
2023-10-27 17:53:42 +0600

    'lm'

---
+++ lessons/lesson7.py
@@ -0,0 +1,29 @@
+# база данных
+# sql
+# СУБД-сис управления базами данных
+#
+# oracle Mysql posgreSQL, NOsql
+# sqlite3
+# CRUD-create reed update delete
+# import sqlite3
+#
+# db = sqlite3.connect('nasa.db')
+#
+# c = db.cursor()
+#
+# c.execute('''CREATE TABLE IF NOT EXISTS user(
+# name TEXT,
+# age DATE,
+# hobby TEXT
+# ) ''')
+#
+# c.execute('''INSERT INTO user VALUES('BEKA','2023-10-2','ertgfvbtfv') ''')
+# c.execute('''UPDATE user SET name='Бека' WHERE rowid>1 ''')
+# c.execute('''DELETE FROM user WHERE rowid>3''')
+# c.execute('''SELECT rowid,* FROM user ''')
+# item = c.fetchall()
+# for i in item:
+#     print(i)
+#
+# db.commit()
+# db.close()
